64e94c92c33a749828716dbb3d6187d3e68121c5
Nhrok <nikolas.t.p123@gmail.com>
2019-03-18 16:21:55 -0300

    added sentiments analyzer

--- models/tweetUserData.js
+++ models/tweetUserData.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const tweetUserDataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user_screen_name: {type: String, required: true},
-    coordinates: {type: Object, required: true}
+    coordinates: {type: Object, required: true},
+    user_text: {type: String, required: true}
 });
 
 module.exports = mongoose.model('tweetUserData', tweetUserDataSchema);
