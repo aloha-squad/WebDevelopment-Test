9f9d76535a58c513928cc30063dce787987f190c
luisinhoferraz <ferrazluisinho@gmail.com>
2019-08-03 18:39:52 -0300

    authorizing requests from react app

--- geofinder/server.js
+++ geofinder/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://tweetlocator.mybluemix.net/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hi! I was created for development test!' });
 });
