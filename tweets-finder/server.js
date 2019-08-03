48abc95870231ea622c59bd53432c09b0b366a77
luisinhoferraz <ferrazluisinho@gmail.com>
2019-08-03 16:29:39 -0300

    adding new route to back-end

--- tweets-finder/server.js
+++ tweets-finder/server.js
@@ -7,4 +7,8 @@ app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hi! I was created for development test!' });
 });
 
+app.get('/', (req, res) => {
+  res.send({ express: `Hello! You're in GeoFinder's Home Page!`});
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
